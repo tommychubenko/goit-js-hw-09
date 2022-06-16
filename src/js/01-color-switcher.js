c4167897b6a25cef41a6ac59c812c9741fc6b60d
Artem Chubenko <artem.chubenko@gmail.com>
2022-06-17 01:13:05 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,32 +1,26 @@
 const body = document.querySelector('body');
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
-let timerId = "";
-
-
+let timerId = '';
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 function onStart() {
-    const color = getRandomHexColor();
-    body.style.backgroundColor = color;
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
-   }
-
-
-
-function onStop() {
-    clearInterval(timerId);
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
-    
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
 }
 
+function onStop() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
 
-
-startBtn.addEventListener('click', setInterval(onStart, 1000));
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(onStart, 1000);
+});
 stopBtn.addEventListener('click', onStop);
